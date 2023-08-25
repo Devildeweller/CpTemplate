231eb7e39d6c762c3043db29675b62788d3f9832
Rajneesh Chaudhary <Chrajneesh15@gmail.com>
2023-08-25 13:59:08 +0530

    Create Simpletemplate.cpp

---
+++ Simpletemplate.cpp
@@ -0,0 +1,29 @@
+/****************************
+Author -> Rajneesh Chaudhary
+****************************/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+#define  endl "\n"
+ 
+const int m=100000007;
+
+
+void solve(){
+	int t; cin >> t;
+	while(t--){
+		
+	}
+}
+ 
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+	solve();
+    
+    return 0;
+}

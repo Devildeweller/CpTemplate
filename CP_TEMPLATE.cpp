aed1fa1475f6d658b8896f2213669411ce4cb01c
Rajneesh Chaudhary <Chrajneesh15@gmail.com>
2023-09-06 10:36:59 +0530

    Create CP_TEMPLATE.cpp

---
+++ CP_TEMPLATE.cpp
@@ -0,0 +1,40 @@
+/****************************
+Author -> Rajneesh Chaudhary
+****************************/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define dbg(...) ; 
+#define debug(...) ; 
+#define crndl ; 
+#endif
+
+#define ll long long
+#define  endl "\n"
+ 
+const int mod=100000007;
+const long long INF=LLONG_MAX >> 1;
+
+void solve(){
+	cout << 2;
+}
+ 
+int32_t main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("io/input.txt", "r", stdin);
+    freopen("io/output.txt", "w", stdout);
+    freopen("io/error.txt", "w", stderr);
+    #endif
+ 
+	int t=1;
+	//cin >> t;
+	while(t--)
+		solve();
+    
+    return 0;
+}

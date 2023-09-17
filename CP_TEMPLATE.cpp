3530a23dfff6650e69b04bacb31c57b01dfb4cfd
Rajneesh Chaudhary <Chrajneesh15@gmail.com>
2023-09-17 21:21:40 +0530

    Update CP_TEMPLATE.cpp

--- CP_TEMPLATE.cpp
+++ CP_TEMPLATE.cpp
@@ -13,28 +13,36 @@ using namespace std;
 #define crndl ; 
 #endif
 
-#define ll long long
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
 #define  endl "\n"
+//#define int long long
  
 const int mod=100000007;
 const long long INF=LLONG_MAX >> 1;
 
 void solve(){
-	cout << 2;
+    
 }
  
 int32_t main(){
+    auto begin = std::chrono::high_resolution_clock::now();
     ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
     #ifndef ONLINE_JUDGE
     freopen("io/input.txt", "r", stdin);
     freopen("io/output.txt", "w", stdout);
     freopen("io/error.txt", "w", stderr);
     #endif
  
-	int t=1;
-	//cin >> t;
-	while(t--)
-		solve();
+    int t=1;
+    //cin >> t;
+    while(t--)
+        solve();
     
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+
     return 0;
 }

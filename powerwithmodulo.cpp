f91e419ffb0d2f3d0ae8f64fd8df1bf176e71652
Rajneesh Chaudhary <Chrajneesh15@gmail.com>
2023-09-18 12:45:40 +0530

    Update powerwithmodulo.cpp

--- powerwithmodulo.cpp
+++ powerwithmodulo.cpp
@@ -1,13 +1,13 @@
-int power(int x, int y, int p)
+int power(int x, int y)
 {
 
     int res = 1;
-    while (y > 0) {
-        if (y % 2 == 1)
-            res = (res * x);
+    while (y) {
+        if (y & 1)
+            res = (res * x)%mod;
         y = y >> 1;
 
-        x = (x * x);
+        x = (x * x)%mod;
     }
-    return res % p;
+    return res % mod;
 }

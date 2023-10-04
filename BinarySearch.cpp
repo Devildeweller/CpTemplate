b32c88b87b72261dd7c9a801c0b62279cc7fcb6e
Rajneesh Chaudhary <Chrajneesh15@gmail.com>
2023-10-04 10:41:00 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -7,3 +7,10 @@ while (r-l > 1) {
     if (a[m] <= t) l = m;
     else r = m;
 }
+
+//binary search for real numbers 
+double long l = 0, r = 1e5+1;
+while (r-l > 1e-7) {
+    double long m = (l + r) / 2;
+    (pred(m) <= t ) ? l = m : r = m;
+}

5e3bc85ba6ed30e73fe4cdaaa861efdd25243279
Rajneesh Chaudhary <Chrajneesh15@gmail.com>
2023-11-21 14:04:42 +0530

    Update CP_TEMPLATE.cpp

--- CP_TEMPLATE.cpp
+++ CP_TEMPLATE.cpp
@@ -1,11 +1,7 @@
-/****************************
-Author -> Rajneesh Chaudhary
-****************************/
- 
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifndef ONLINE_JUDGE
+#ifdef DevilDeweller
 #include "debug.h"
 #else
 #define dbg(...) ; 
@@ -14,31 +10,45 @@ using namespace std;
 #endif
 
 mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
-
-#define  endl "\n"
-//#define int long long
  
-const int mod=100000007;
+#define  endl "\n"
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define sz(x) (int) (x).size()
+
+#define f first
+#define s second
+#define mp make_pair
+//#define int long long
+
+using vi = vector<int>;
+using pi = pair<int,int>;
+ 
+const int mod=1e9+7;
 const long long INF=LLONG_MAX >> 1;
 
 void solve(){
-    
+
 }
  
 int32_t main(){
     auto begin = std::chrono::high_resolution_clock::now();
     ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-    #ifndef ONLINE_JUDGE
-    freopen("io/input.txt", "r", stdin);
-    freopen("io/output.txt", "w", stdout);
-    freopen("io/error.txt", "w", stderr);
+    #ifdef DevilDeweller
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
     #endif
  
     int t=1;
-    //cin >> t;
-    while(t--)
+    cin >> t;
+    for(int _=1;_<=t;_++){
+        debug(TestCase,_);
         solve();
+        cout << endl;
+        crndl;
+    }
     
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
